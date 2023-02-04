3615e2719b8461204998dfddc7ae2b40cd59ae8c
clayton-klein <csklein87@hotmail.com>
2023-02-04 15:47:08 -0300

    refactor: add Card components dynamically
    I also added 2 other photos for the other cards and a 'data.js' file, wich serves kind of a dummy data base with de info to fill de cards.

--- src/App.js
+++ src/App.js
@@ -2,20 +2,28 @@ import "./App.css";
 import Navbar from "./components/Navbar.js";
 import Hero from "./components/Hero.js";
 import Card from "./components/Card.js";
+import cardsData from "./data.js";
 
 function App() {
+  const cardsArray = cardsData.map((card) => {
+    return (
+      <Card
+        key={card.id}
+        img={card.coverImg}
+        rating={card.stats.rating}
+        reviewCount={card.stats.reviewCount}
+        country={card.location}
+        title={card.title}
+        price={card.price}
+      />
+    )
+  });
+
   return (
     <div className="App">
       <Navbar />
       <Hero />
-      <Card
-        img="katie-zaferes.png"
-        rating={5.0}
-        reviewCount={6}
-        country="USA"
-        title="Life lessons with Katie Zaferes"
-        price={136}
-      />
+      {cardsArray}
     </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,20 +1,13 @@
 import "../styles/Card.css";
-// import starIcon from ".  ./images/star-icon.svg";
-// import photo from '../images/katie-zaferes.png'
 
 export default function Card(props) {
-  console.log(props)
   return (
     <section className="card-container">
       <div className="card">
         <img
           className="card__img"
-          // according to the course the solution bellow should work,
-          // but idk why it's not, so I went back to the hard coded 
-          // image for now
-          src={`./${props.img}`}
-          // src={photo}
-          alt="katie zaferes usando maiô em uma competição"
+          src={`./images/${props.img}`}
+          alt="alt description here"
         />
         <p className="card__tag">SOLD OUT</p>
         <p className="card__reviews">
